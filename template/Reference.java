af894d439244131eba7be9be632648e016bfe9d1
NewMaa <slmajlapeng@163.com>
2025-08-16 16:31:25 +0800

    1.0.0 - preparing

--- template/Reference.java
+++ template/Reference.java
@@ -5,8 +5,8 @@ package $package;
  */
 public class Reference {
 
-    public static final String MOD_ID = "$mod_id";
-    public static final String MOD_NAME = "$mod_name";
-    public static final String VERSION = "$mod_version";
+    public static final String MOD_ID = "othtech";
+    public static final String MOD_NAME = "123Technology - Nova Engineering";
+    public static final String VERSION = "1.0.0";
 
 }
