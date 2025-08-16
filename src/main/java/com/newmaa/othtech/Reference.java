0464e99cf5154f3570667c9b46e34ced67a89c55
NewMaa <slmajlapeng@163.com>
2025-08-16 18:53:00 +0800

    1.0.0 - preparing 1

---
+++ src/main/java/com/newmaa/othtech/Reference.java
@@ -0,0 +1,12 @@
+package com.newmaa.othtech;
+
+/**
+ * Tags storage class, you can change at will
+ */
+public class Reference {
+
+    public static final String MOD_ID = "othtech";
+    public static final String MOD_NAME = "123Technology - Nova Engineering";
+    public static final String VERSION = "1.0.0";
+
+}
